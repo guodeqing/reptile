83a1eaae78a69117df6265d5a8c8a1b98d1a9614
guodeqing <1195799195@qq.com>
2019-10-09 00:08:47 +0800

    tomcat remove

--- src/main/java/reptile/ApplicationMain.java
+++ src/main/java/reptile/ApplicationMain.java
@@ -2,9 +2,17 @@ package reptile;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+@ServletComponentScan
 @SpringBootApplication
-public class ApplicationMain {
+public class ApplicationMain extends SpringBootServletInitializer{
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(this.getClass());
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ApplicationMain.class, args);
 	}
